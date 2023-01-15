a4ddf46a20986c5f708a041e6ec1ddf2a1f2f571
Hardik Raina <hardikraina079@gmail.com>
2023-01-15 12:12:25 +0530

    Can now specify input mode
    Added CLI functionality to specify running mode between a specified video file path or the default camera.

--- running.py
+++ running.py
@@ -1,36 +1,34 @@
+import sys
 import cv2
 import numpy as np
 from keras.models import load_model
 
 model = load_model('pose_classification_model.h5')
 
-num_frames = 5
+# Uses camera by default if no video path provided
+if len(sys.argv) == 1: 
+    video = cv2.VideoCapture(0)
+else:
+    video = cv2.VideoCapture(sys.argv[1])
 
-video = cv2.VideoCapture('example.mp4')
-
-prediction_buffer = []
-
-while True:
+while True: 
     ret, frame = video.read()
+
     if not ret: break
 
+    # Preprocessing
     frame = cv2.resize(frame, (224, 224))
     frame = frame.astype('float32') / 255.0
     frame = np.expand_dims(frame, axis=0)
 
     prediction = model.predict(frame)
 
-    prediction_buffer.append(prediction)
+    class_idx = np.argmax(prediction)
 
-    if len(prediction_buffer) > num_frames: prediction_buffer.pop(0)
-    average_prediction = np.mean(prediction_buffer, axis=0)
+    # Going to change print to speaking in next version
+    print(class_idx)                   
 
-    class_idx = np.argmax(average_prediction)
-    print(class_idx)
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the video file and close the window
 video.release()
 cv2.destroyAllWindows()
